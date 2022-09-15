3ed4a96f32425dd6daf1dadaf827d035dd150d17
Jgomez <jgomez@sistecredito.com>
2022-09-15 15:04:03 -0500

    Chapter 12: Working with JsonPath

--- src/test/java/com/herokuapp/restfullbooker/GetBookingTests.java
+++ src/test/java/com/herokuapp/restfullbooker/GetBookingTests.java
@@ -4,15 +4,13 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-import java.util.List;
-/*
-        CHAPTER 11 HOMEWORK
- */
+
 public class GetBookingTests {
 
-    @Test
-    public void getBookingNameAndLastName(){
+    @Test //CHAPTER 11 HOMEWORK
+     public void getBookingNameAndLastName(){
 
         // Get response from GetBooking
 
@@ -30,6 +28,51 @@ public class GetBookingTests {
 
         Assert.assertEquals(response.jsonPath().getString("lastname"),
                 "Morante Briones",
-                "The lasname should be 'Morante Briones' but it is not");
+                "The lastname should be 'Morante Briones' but it is not");
+    }
+    @Test
+    public void getBookingTests(){
+        // Get response with booking
+        Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking/210");
+        response.print();
+        //Verify response 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Expected 200 but it is not");
+        //Verify All fields
+        SoftAssert softAssert = new SoftAssert();
+
+        String actualFirstName = response.jsonPath().getString("firstname");
+        softAssert.assertEquals(actualFirstName,"Howard", "firstname in response is not expected");
+
+        String actualLastName = response.jsonPath().getString("lastname");
+        softAssert.assertEquals(actualLastName,"Morante Briones", "lastname in response is not expected");
+
+        int actualTotalPrice = response.jsonPath().getInt("totalprice");
+        softAssert.assertEquals(actualTotalPrice,111, "totalprice in response is not expected");
+
+        boolean actualDepositePaid = response.jsonPath().getBoolean("depositpaid");
+        softAssert.assertTrue(actualDepositePaid, "depositpaid should be true but it is not");
+
+        String actualCheckin = response.jsonPath().getString("bookingdates.checkin");
+        softAssert.assertEquals(actualCheckin, "2018-01-01", "checkin in response is not expected");
+
+        String actualCheckout = response.jsonPath().getString("bookingdates.checkout");
+        softAssert.assertEquals(actualCheckout, "2019-01-01", "checkout in response is not expected");
+
+        softAssert.assertAll();
+
+        /*
+        {
+            "firstname":"Howard",
+            "lastname":"Morante Briones",
+            "totalprice":111,
+            "depositpaid":true,
+            "bookingdates":
+                {
+                    "checkin":"2018-01-01",
+                    "checkout":"2019-01-01"
+                },
+            "additionalneeds":"Breakfast"
+        }
+         */
     }
 }
