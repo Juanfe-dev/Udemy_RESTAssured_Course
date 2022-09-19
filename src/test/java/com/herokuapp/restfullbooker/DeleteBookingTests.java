088fd5d26c4c886469fb60f55665c91485374f86
Jgomez <jgomez@sistecredito.com>
2022-09-19 11:23:33 -0500

    Chapter 17: Automating DELETE request

---
+++ src/test/java/com/herokuapp/restfullbooker/DeleteBookingTests.java
@@ -0,0 +1,50 @@
+package com.herokuapp.restfullbooker;
+
+import com.herokuapp.BaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DeleteBookingTests extends BaseTest {
+
+    @Test
+    public void createBookintTest(){
+
+        //Create Json Body -> Create a booking
+        Response responseCreate = createBooking();
+        responseCreate.print();
+
+        //Get booking of the new booking
+        int bookingid = responseCreate.jsonPath().getInt("bookingid");
+
+        //Delete booking
+        //Change post for put
+        //Autorization
+        //    "username" : "admin",
+        //    "password" : "password123"
+        //Add .auth().preemptive().basic(username, password) for authentication
+        Response responsePartialUpdate = RestAssured
+                .given().auth().preemptive().basic("admin","password123")
+                //.post("https://restful-booker.herokuapp.com/booking");
+                //.put("https://restful-booker.herokuapp.com/booking/"+bookingid);
+                //.patch("https://restful-booker.herokuapp.com/booking/"+bookingid);
+                .delete("https://restful-booker.herokuapp.com/booking/"+bookingid);
+        responsePartialUpdate.print();
+
+        //Verifications
+            //Verify response 201
+            Assert.assertEquals(responsePartialUpdate.getStatusCode(), 201, "Expected 201 but it is not");
+
+            //Response the endpoint
+            Response responseGet = RestAssured.get("https://restful-booker.herokuapp.com/booking/"+bookingid);
+            responseGet.print();
+
+            //Response body as string
+            Assert.assertEquals(responseGet.getBody().asString(),
+                    "Not Found",
+                    "Body should be 'Not Found'");
+    }
+
+
+}
