789f016f7c42cb38a326c7fa101d9079c2787142
Jgomez <jgomez@sistecredito.com>
2022-09-19 17:15:42 -0500

    Chapter 21: Headers and Cookies

--- src/test/java/com/herokuapp/restfullbooker/HealthCheckTests.java
+++ src/test/java/com/herokuapp/restfullbooker/HealthCheckTests.java
@@ -1,6 +1,12 @@
 package com.herokuapp.restfullbooker;
 
 import com.herokuapp.BaseTest;
+import io.restassured.RestAssured;
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -24,5 +30,41 @@ public class HealthCheckTests extends BaseTest {
         .then()
             .assertThat()
             .statusCode(201);
+    }
+    @Test //CHAPTER 21: Headers and Cookies
+    public void headersAndCookies(){
+        //We don't have cookies to work.
+        //We will do the encoding simulating that we have cookies.
+
+        Header someHeader = new Header("Some name", "Some value");
+        spec.header(someHeader);
+
+        Cookie someCookie = new Cookie.Builder("Some Cookie", "Some Cookie Value").build();
+        spec.cookie(someCookie);
+
+        Response response = RestAssured.given(spec)
+                .cookie("Test cookie name", "Test cookie value")
+                .header("Test header name", "Test header value").log().all()
+                .get("/ping");
+
+        //Get Headers
+        Headers headers = response.getHeaders();
+        System.out.println("Headers: "+headers);
+
+        //First way to print a specific header
+        Header serverHeader1 = headers.get("Server");
+        System.out.println(serverHeader1.getName() + ": " + serverHeader1.getValue());
+
+        //Second way to print a specific header
+        String serverHeader2 = response.getHeader("Server");
+        System.out.println("Server: " + serverHeader2);
+
+        //Get Cookies
+        Cookies cookies = response.getDetailedCookies();
+        System.out.println("Cookies: "+ cookies);
+
+
+
+
     }
 }
